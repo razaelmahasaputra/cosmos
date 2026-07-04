8d4a3fe2a29a1ae8456cb40dc2afda4e97a3ca10
razaeldotexe <razaelsaputra863@gmail.com>
2026-07-05 04:49:12 +0700

    Add specific INFO logging for web_search success

--- src/tools/web_search.js
+++ src/tools/web_search.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { writeLog } from '../logger.js';
 export const definition = {
     name: 'web_search',
     description: 'Mencari informasi terbaru atau real-time dari internet (seperti berita, cuaca terkini yang kompleks, harga, atau fakta spesifik).',
@@ -27,6 +27,8 @@ export async function execute({ query }, ctx) {
             include_answer: true,
             max_results: 3
         });
+
+        writeLog('INFO', `[web_search] Success getting results from Tavily`, { query, resultsCount: response.data.results?.length });
 
         if (response.data && response.data.answer) {
             return `Hasil Pencarian untuk "${query}":\n\n${response.data.answer}`;
