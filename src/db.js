8460b5ab67673b14ba4ef25b751a2eeff8829a24
razaeldotexe <razaelsaputra863@gmail.com>
2026-07-04 17:23:15 +0700

    Update Supabase key to use service role

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 export const supabase = supabaseUrl && supabaseKey ? createClient(supabaseUrl, supabaseKey) : null;
 
