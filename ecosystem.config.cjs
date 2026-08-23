96ecb898457270425d6395e11c46083275e9785b
razaeldotexe <razaelsaputra863@gmail.com>
2026-08-23 02:46:04 +0000

    chore: add dedicated pm2 log file paths

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
       name: "waf-bot",
       script: "dist/index.js",
       watch: false,
+      out_file: "./storage/logs/waf-bot-out.log",
+      error_file: "./storage/logs/waf-bot-error.log",
+      merge_logs: true,
+      time: true,
       env: {
         NODE_ENV: "production",
       }
