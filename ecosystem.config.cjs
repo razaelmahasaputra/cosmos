49a1cb27e9491f2e0732583b136d8041ad2662e4
razaeldotexe <razaelsaputra863@gmail.com>
2026-08-24 07:01:46 +0000

    chore: add memory limits and PM2 max_memory_restart rule to optimize memory usage

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,6 +9,8 @@ module.exports = {
             error_file: './storage/logs/waf-bot-error.log',
             merge_logs: true,
             time: true,
+            node_args: '--max-old-space-size=256',
+            max_memory_restart: '300M',
             env: {
                 NODE_ENV: 'production'
             }
